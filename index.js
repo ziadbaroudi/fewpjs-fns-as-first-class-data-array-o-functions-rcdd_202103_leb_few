e21c5b766b108159617032d768aafaf6df6efa70
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-14 00:35:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ var unleashDog = function(dogName="Byron", dogBreed="poodle") {
   return `Unleash ${dogName} the ${dogBreed}`
 }
 
-// const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
-// function exerciseDog(dog, breed) {
-//   return routine.map(fn => fn(dog, breed))
-// }
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed))
+}
